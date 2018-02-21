430e22437f316f97e0bdb19bd1ed50ad230e9415
Maciek Stasieluk <macrusher@gmail.com>
2018-02-21 10:28:15 +0100

    Change message order

--- handler.ts
+++ handler.ts
@@ -4,7 +4,7 @@ import {parse} from 'qs';
 
 export const anonymous: Handler = (event: APIGatewayEvent, context: Context, cb: Callback) => {
     // Get only the message text from the request and ignore other metadata to keep it private
-    const {text = '', response_url} = parse(event.body || '');
+    const {text = '', response_url = ''} = parse(event.body || '');
 
     if (!text || !response_url) {
         cb(null, {
@@ -17,6 +17,16 @@ export const anonymous: Handler = (event: APIGatewayEvent, context: Context, cb:
         return;
     }
 
+    // Respond to the original request
+    cb(null, {
+        body: JSON.stringify({
+            response_type: 'ephemeral',
+            text: 'Twoja wiadomość zostanie opublikowana anonimowo.\n' +
+            'Dziękujemy za skorzystanie z usług AnonimowychVazcoPytań :ghost:',
+        }),
+        statusCode: 200,
+    });
+
     // Reply with the same text in the same channel, but anonymously
     fetch(response_url, {
         body: JSON.stringify({
@@ -28,14 +38,4 @@ export const anonymous: Handler = (event: APIGatewayEvent, context: Context, cb:
         },
         method: 'POST',
     }).catch(err => console.error(err));
-
-    // Respond to the original request
-    cb(null, {
-        body: JSON.stringify({
-            response_type: 'ephemeral',
-            text: 'Twoja wiadomość zostanie opublikowana anonimowo.\n' +
-            'Dziękujemy za skorzystanie z usług AnonimowychVazcoPytań :ghost:',
-        }),
-        statusCode: 200,
-    });
 };
